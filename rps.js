c9e214ada353ae079b8da0ed1c637b2359796b9a
GraceLee-ada <grace.lee@ada.ac.uk>
2024-09-25 13:46:29 +0000

    Passed all tests for the first time!

--- rps.js
+++ rps.js
@@ -1,17 +1,45 @@
 function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw";
-  } else {
-    
+  };
+  switch (player1) {
+    case "rock":
+      if (player2 === "paper" || player2 === "spock") {
+        return "player2"
+      } else {
+        return "player1";
+      }
+    case "paper":
+      if (player2 === "scissors" || player2 ==="lizard") {
+        return "player2";
+      }else {
+        return "player1";
+      }
+    case "scissors":
+      if (player2 === "rock" || player2 ==="spock") {
+        return "player2";
+      }else {
+        return "player1";
+      }
+    case "lizard":
+      if (player2 === "rock" || player2 ==="scissors") {
+        return "player2";
+      }else {
+        return "player1";
+      }
+    case "spock":
+      if (player2 === "lizard" || player2 ==="paper") {
+        return "player2";
+      }else {
+        return "player1";
+      }
+  };
   }
-  
-};
 
-const rock = ["scissors", "lizard", "spock", "paper"]
-const paper = ["rock", "spock", "scissors", "lizard"]
-const scissors = ["paper", "lizard", "spock", "rock"]
-const lizard = ["paper", "spock", "rock", "scissors"]
-const spock = ["scissors", "rock", "lizard", "paper"]
+// Leave this code here for the automated tests
+module.exports = {
+  rockPaperScissors,
+}
 
 
 
@@ -20,38 +48,7 @@ const spock = ["scissors", "rock", "lizard", "paper"]
 
 
 
-
-
-console.log(rockPaperScissors("paper", "lizard"));
-
-// switch (player1) {
-//   case "rock":
-//     if (player2 === "paper" && "spock") {
-//       return "player2"
-//     };
-//     return "player1";
-//   case "paper":
-//     if (player2 === "scissors" && "lizard") {
-//       return "player2";
-//     };
-//     return "player1";
-//   case "scissors":
-//     if (player2 === "rock" && "spock") {
-//       return "player2";
-//     };
-//     return "player1";z
-//   case "lizard":
-//     if (player2 === "rock" && "scissors") {
-//       return "player2";
-//     };
-//     return "player1";
-//   case "spock":
-//     if (player2 === "lizard" && "paper") {
-//       return "player2";
-//     };
-//     return "player1";
-// };
-
+// //if...else method
 // if (player1 === player2) {
 //   return "draw";
 // } else {
@@ -70,10 +67,64 @@ console.log(rockPaperScissors("paper", "lizard"));
 //         return "player2";
 //       };
 //       return "player1";
-//   } 
+//   } else if (player1 === "lizard") {
+//     if (player2 === "rock") {
+//       return "player2";
+//     };
+//     return "player1";
+//   } else if (player1 === "spock") {
+//     if (player2 === "paper") {
+//       return "player2";
+//     };
+//     return "player1";
+//   }; 
 // };
 
-// Leave this code here for the automated tests
-module.exports = {
-  rockPaperScissors,
-}
+// //switch...case method
+// switch (player1) {
+// case "rock":
+//   if (player2 === "paper" && "spock") {
+//     return "player2"
+//   };
+//   return "player1";
+// case "paper":
+//   if (player2 === "scissors" && "lizard") {
+//     return "player2";
+//   };
+//   return "player1";
+// case "scissors":
+//   if (player2 === "rock" && "spock") {
+//     return "player2";
+//   };
+//   return "player1";
+// case "lizard":
+//   if (player2 === "rock" && "scissors") {
+//     return "player2";
+//   };
+//   return "player1";
+// case "spock":
+//   if (player2 === "lizard" && "paper") {
+//     return "player2";
+//   };
+//   return "player1";
+// };
+
+// //Idea 1
+// if (player1 === player2) {
+// return "draw";
+// };
+// for (let i of choices) {
+// if (i.includes(i.slice(0,1)) === true) {
+//   return "player1";
+// } else {
+//   return "player2";
+// };
+// };
+
+// let choices = {
+// rock: ["scissors", "lizard", "spock", "paper"],
+// paper:["rock", "spock", "scissors", "lizard"],
+// scissors: ["paper", "lizard", "spock", "rock"],
+// lizard: ["paper", "spock", "rock", "scissors"],
+// spock: ["scissors", "rock", "lizard", "paper"],
+// };
