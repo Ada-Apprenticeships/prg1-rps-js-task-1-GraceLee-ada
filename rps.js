cca23198b26dec717c98e0d62719b1f7b0ece008
GraceLee-ada <grace.lee@ada.ac.uk>
2024-09-25 09:50:34 +0000

    3 tests passed, code before refactoring

--- rps.js
+++ rps.js
@@ -2,8 +2,21 @@ function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw";
   } else {
-    if ((player1 === "rock") {
-    
+    if (player1 === "rock") {
+      if (player2 === "paper") {
+        return "player2";
+      };
+      return "player1";
+    } else if (player1 === "paper") {
+        if (player2 === "scissors") {
+          return "player2";
+        };
+        return "player1";
+    } else {
+        if (player2 === "scissors") {
+          return "player2";
+        };
+        return "player1";
     }
   };
 };
