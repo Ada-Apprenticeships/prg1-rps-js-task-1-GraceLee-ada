d5b3aa9378b4f104bf8fef3b3e0cfdd4278cc6eb
GraceLee-ada <grace.lee@ada.ac.uk>
2024-09-25 15:29:16 +0000

    Passed all tests, improved

--- rps.js
+++ rps.js
@@ -1,19 +1,19 @@
-function rockPaperScissors(player1, player2) {
-  if (player1 === player2) {
-    return "draw";
-  } else if (choices[player1].includes(player2) === true) {
-    return "player1";
-  }
-  return "player2";
-}
-
-let choices = {
+const winningOptions = {
   rock: ["scissors", "lizard"],
   paper: ["rock", "spock"],
   scissors: ["paper", "lizard"],
   lizard: ["paper", "spock"],
   spock: ["scissors", "rock"],
 };
+
+function rockPaperScissors(player1, player2) {
+  if (player1 === player2) {
+    return "draw";
+  } else if (winningOptions[player1].includes(player2)) {
+    return "player1";
+  }
+  return "player2";
+}
 
 // Leave this code here for the automated tests
 module.exports = {
